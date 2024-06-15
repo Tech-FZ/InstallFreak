9bbddfb288e9f89b4a40c3170f1ede3c92316f64
Nicolas <nicolas62974@gmail.com>
2024-06-15 10:40:40 +0200

    Remove faulty line from succeed window script

--- IF_RequiredScripts/IF_Succeeded.ps1
+++ IF_RequiredScripts/IF_Succeeded.ps1
@@ -1,4 +1,3 @@
 Add-Type -AssemblyName PresentationFramework
 
-$null = Show-MessageBox "InstallFreak was able to install EmuGUI. Please press OK to close the window." "InstallFreak - Setup successful"
 [System.Windows.MessageBox]::Show('InstallFreak was able to install EmuGUI. Please press OK to close the window.', 'InstallFreak - Setup successful', 'OK', 'Information')
